f950ccd1219c241151e5171cecc4118a3e284ad4
Nikhilesh-H <nikgrinding@gmail.com>
2025-05-15 18:46:08 +0530

    15/05/2025

--- AOC15/day-02/1.py
+++ AOC15/day-02/1.py
@@ -2,7 +2,12 @@ test_file = r"AOC\AOC15\day-02\test.txt"
 input_file = r"AOC\AOC15\day-02\input.txt"
 
 def part_1(ip):
-    pass
+    ip = ip.split("x")
+    ip = [int(i) for i in ip]
+    area_1 = ip[0]*ip[1]
+    area_2 = ip[1]*ip[2]
+    area_3 = ip[2]*ip[0]
+    return 2*(area_1+area_2+area_3) + min(area_1, area_2, area_3)
 
 with open(test_file) as f:
     part1_ip = f.read().split("\n\n")[0]
@@ -15,4 +20,6 @@ with open(test_file) as f:
         print("Generated Output:", part_1(ip))
     
 with open(input_file) as f:
-    print ("\nPart - 1: Main", part_1(f.read()))
+    part1_ip = f.read().split("\n")
+    op = [part_1(ip) for ip in part1_ip]
+    print ("\nPart - 1: Main", sum(op))
---
+++ AOC15/day-02/2.py
@@ -0,0 +1,25 @@
+test_file = r"AOC\AOC15\day-02\test.txt"
+input_file = r"AOC\AOC15\day-02\input.txt"
+
+def part_2(ip):
+    ip = ip.split("x")
+    ip = [int(i) for i in ip]
+    area_1 = 2*(ip[0]+ip[1])
+    area_2 = 2*(ip[1]+ip[2])
+    area_3 = 2*(ip[2]+ip[0])
+    return ip[0]*ip[1]*ip[2] + min(area_1, area_2, area_3)
+
+with open(test_file) as f:
+    part2_ip = f.read().split("\n\n")[1]
+    part2_ip = part2_ip.split("\n")
+    print("Part - 2: Test")
+    for i in part2_ip:
+        ip = i.split()[0]
+        print("\nInput:", ip)
+        print("Expected Output:", i.split()[1])
+        print("Generated Output:", part_2(ip))
+    
+with open(input_file) as f:
+    part2_ip = f.read().split("\n")
+    op = [part_2(ip) for ip in part2_ip]
+    print ("\nPart - 2: Main", sum(op))
