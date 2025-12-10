c793ae2c162979febd973507547292a203072010
Nikhilesh H <nikgrinding@gmail.com>
2025-12-10 21:44:28 +0530

    09/12/2025

--- AOC25/day-09/1.py
+++ AOC25/day-09/1.py
@@ -12,7 +12,7 @@ def part_1(ip):
 with open(test_file) as f:
     part_1_ip, part_1_op = f.read().split('\n\n\n')[0].split('\n\n')
     part_1_ip = part_1_ip.split("\n")
-    print("Part - 2: Test")
+    print("Part - 1: Test")
     print("\nInput:")
     for i in part_1_ip:
         print(i)
@@ -20,4 +20,5 @@ with open(test_file) as f:
     print("Generated Output:", part_1(part_1_ip))
     
 with open(input_file) as f:
-    print ("\nPart - 2: Main", part_1(f.read().split("\n")))
+    print ("\nPart - 1: Main", part_1(f.read().split("\n")))
+
